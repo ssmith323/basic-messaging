3c9a225c2beb829c2df7c899a840b15c07521e3d
s.j.smith <s.j.smith@accenture.com>
2019-03-20 17:01:17 -0400

    add unittest

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,17 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { MessageService } from '../shared/message.service';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockMessageService;
 
   beforeEach(async(() => {
+    mockMessageService = jasmine.createSpyObj(['create']);
+
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule, RouterTestingModule],
+      providers: [
+        { provide: MessageService, useValue: mockMessageService }
+      ]
     })
       .compileComponents();
   }));
@@ -32,5 +40,13 @@ describe('HomeComponent', () => {
 
   it('should have a submit button', () => {
     expect(fixture.debugElement.query(By.css('button#submit'))).toBeTruthy();
+  });
+
+  it('should save the message when submit is clicked', () => {
+    const navigateSpy = spyOn((component as any).router, 'navigate');
+    component.messageForm.message = 'Hi';
+    fixture.debugElement.query(By.css('button#submit')).triggerEventHandler('click', null);
+    expect(mockMessageService.create).toHaveBeenCalledWith('Hi');
+    expect(navigateSpy).toHaveBeenCalledWith(['/success']);
   });
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  messageForm: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
